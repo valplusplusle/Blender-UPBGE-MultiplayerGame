fbcb761e00bd571ab47496f1ce59fcdf03e9a686
Valentin Kummert <valentin@innosonix.de>
2020-05-03 02:07:17 +0200

    small network bugfix

--- PythonScripts/clientGetOtherPlayerPositions.py
+++ PythonScripts/clientGetOtherPlayerPositions.py
@@ -35,26 +35,41 @@ Matrix[1][1] = msglist[5]
 Matrix[1][2] = msglist[6]
 Matrix[1][3] = msglist[7]
 
-Matrix[1][0] = msglist[8]
-Matrix[1][1] = msglist[9]
-Matrix[1][2] = msglist[10]
-Matrix[1][3] = msglist[11]
+Matrix[2][0] = msglist[8]
+Matrix[2][1] = msglist[9]
+Matrix[2][2] = msglist[10]
+Matrix[2][3] = msglist[11]
 
-Matrix[1][0] = msglist[12]
-Matrix[1][1] = msglist[13]
-Matrix[1][2] = msglist[14]
-Matrix[1][3] = msglist[15]
+Matrix[3][0] = msglist[12]
+Matrix[3][1] = msglist[13]
+Matrix[3][2] = msglist[14]
+Matrix[3][3] = msglist[15]
 
 controller = bge.logic.getCurrentController()
 obj = controller.owner
 scene = bge.logic.getCurrentScene()
 playerID = str(obj['ID'])
 
-test = 1
-for i in range(len(Matrix)):
-    if Matrix[i][0] != playerID:
-        controller = bge.logic.getCurrentController()
-        obj = controller.owner
-        scene = bge.logic.getCurrentScene()
-        scene.objects["Cube.00"+str(test)].localPosition = [float(Matrix[i][1]), float(Matrix[i][2]), float(Matrix[i][3])]
-        test = test +1
+if Matrix[0][0] != playerID:
+    controller = bge.logic.getCurrentController()
+    obj = controller.owner
+    scene = bge.logic.getCurrentScene()
+    scene.objects["Cube.000"].localPosition = [float(Matrix[0][1]), float(Matrix[0][2]), float(Matrix[0][3])]
+
+if Matrix[1][0] != playerID:
+    controller = bge.logic.getCurrentController()
+    obj = controller.owner
+    scene = bge.logic.getCurrentScene()
+    scene.objects["Cube.001"].localPosition = [float(Matrix[1][1]), float(Matrix[1][2]), float(Matrix[1][3])]
+
+if Matrix[2][0] != playerID:
+    controller = bge.logic.getCurrentController()
+    obj = controller.owner
+    scene = bge.logic.getCurrentScene()
+    scene.objects["Cube.002"].localPosition = [float(Matrix[2][1]), float(Matrix[2][2]), float(Matrix[2][3])]
+
+if Matrix[3][0] != playerID:
+    controller = bge.logic.getCurrentController()
+    obj = controller.owner
+    scene = bge.logic.getCurrentScene()
+    scene.objects["Cube.003"].localPosition = [float(Matrix[3][1]), float(Matrix[3][2]), float(Matrix[3][3])]
